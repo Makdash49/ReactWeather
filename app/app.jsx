d698d9080a8ee23bced17f97a74495d3056e7f23
Mark McQuillen <markdmcq@hotmail.com>
2016-10-11 12:49:15 -0700

    show main component after a struggle with createClass syntax

--- app/app.jsx
+++ app/app.jsx
@@ -1,13 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var Main = require('Main');
 
 ReactDOM.render(
-  <h1>Boilerplate app!</h1>,
+  <Router history={hashHistory}>
+    <Route path="/" component={Main}>
+    </Route>
+  </Router>,
   document.getElementById('app')
 );
-
-//Changes to greeter
-// 1. Add message to getInitialState (get default message from props)
-// 2. handleNewName function will take the updates as its variable
-// 3. render function of Greeter needs to use state instead of the props
-// 4. Use placeholder ref for placeholder text.
