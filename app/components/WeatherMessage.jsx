b5b138a36ad900ab4bc6f68c2b620245bc1d46ed
Mark McQuillen <markdmcq@hotmail.com>
2016-10-12 11:27:42 -0700

    solve fetching data false challenge

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,8 +2,7 @@ var React = require('react');
 
 var WeatherMessage = React.createClass({
   render: function () {
-    var temp = this.props.temp;
-    var location = this.props.location;
+    var {temp, location} = this.props;
     return (
       <h2>It's {temp} in {location}.</h2>
     );
