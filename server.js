38c84c38ee2d585d08d3cd7819a443cb58a6ae97
Mark McQuillen <markdmcq@hotmail.com>
2016-10-13 17:11:42 -0700

    rename example page

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
+
+// COMMENT THIS OUT FOR LOCAL HOST:
+
 app.use(function (req, res, next) {
   if (req.headers['x-forwarded-proto'] === 'http') {
     next();
@@ -12,6 +15,8 @@ app.use(function (req, res, next) {
     res.redirect('http://' + req.hostname + req.url);
   }
 });
+///////////////////////
+
 
 app.use(express.static('public'));
 
