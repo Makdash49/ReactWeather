fc732ffdf7638b79ac328997415cd9533bb829fe
Mark McQuillen <markdmcq@hotmail.com>
2016-10-14 13:38:24 -0700

    added content and formatting to About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,27 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = (props) => {
-  return (
+  return  (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page!</p>
+      <h1 className="text-center">About</h1>
+      <p>
+        This is a weather application built on React. I have built this for the
+        Complete React Web App Developer Course.
+      </p>
+
+      <p>Here are relevant links:</p>
+      <ul>
+        <li>
+          <a href='//github.com/Makdash49/ReactWeather'>ReactWeather</a> -
+            This is the repo in GitHub.
+        </li>
+        <li>
+          <a href='//openweathermap.org/'>Open Weather Map</a> - I used Open
+            Weather Map to search for weather data by city name.
+
+        </li>
+      </ul>
     </div>
   )
 };
